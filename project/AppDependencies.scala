699e68f5cb70d9fe1527d02f87cd513c1e1b9e23
daglisood <88654936+daglisood@users.noreply.github.com>
2021-08-31 18:28:30 +0100

    DLS-4476: Adding missing dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.3.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.12.0",
     "uk.gov.hmrc"       %% "stub-data-generator"        % "0.5.3",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.4",
-    "com.github.java-json-tools" % "json-schema-validator" % "2.2.14",
+    "com.github.java-json-tools" % "json-schema-validator" % "2.2.14"
   )
 
   val test = Seq(
     "org.scalatest" %% "scalatest" % "3.2.9" % "test",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0" % Test ,
     "org.pegdown"    % "pegdown"   % "1.6.0" % "test"
   )
 
