8102fe807bc9f5dba979c24e4f4495b673a8409b
daglisood <88654936+daglisood@users.noreply.github.com>
2021-09-06 15:15:30 +0100

    DLS-4476: Upgrading to play 2.8

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-26"  % "5.3.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.12.0",
     "uk.gov.hmrc"       %% "stub-data-generator"        % "0.5.3",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.9.5"
   )
 
   val test = Seq(
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0" % Test ,
     "org.scalatest" %% "scalatest" % "3.0.9" % "test",
     "org.pegdown"    % "pegdown"   % "1.6.0" % "test"
   )
