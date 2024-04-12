f0d65981f2a34d8393859405356ddedd69edd788
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-04-12 12:25:39 +0100

    DLS-10323: Tidied up dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootStrapVersion,
-    "com.eclipsesource" %% "play-json-schema-validator" % "0.9.5"
+    "com.eclipsesource" %% "play-json-schema-validator" % "0.9.5",
+    "uk.gov.hmrc"       %% "stub-data-generator"        % "1.1.0",
+    "uk.gov.hmrc"       %% "tax-year"                   % "4.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "tax-year"               % "4.0.0",
-    "uk.gov.hmrc"            %% "stub-data-generator"    % "1.1.0",
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.36.8"         % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % Test,
-    "org.scalatest"          %% "scalatest"              % "3.0.9"          % "test",
-    "org.pegdown"             % "pegdown"                % "1.6.0"          % "test",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootStrapVersion % Test
   )
 }
