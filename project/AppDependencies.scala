eaa9bb3131e83e0c9b3348e0f91b356c493e5f78
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2024-04-26 10:13:32 +0100

    DLS-10391 - reformatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ object AppDependencies {
   val bootStrapVersion = "8.3.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootStrapVersion,
-    "uk.gov.hmrc"       %% "stub-data-generator"        % "1.1.0",
-    "uk.gov.hmrc"       %% "tax-year"                   % "4.0.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootStrapVersion,
+    "uk.gov.hmrc" %% "stub-data-generator"       % "1.1.0",
+    "uk.gov.hmrc" %% "tax-year"                  % "4.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
