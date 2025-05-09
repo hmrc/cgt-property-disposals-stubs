91a2cb168e9a2217e67460de60b4f70b6b563eea
johno1985 <1640705+johno1985@users.noreply.github.com>
2025-05-09 12:48:07 +0100

    DLS-11614: Update tax-year to 6.0.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootStrapVersion,
     "uk.gov.hmrc" %% "stub-data-generator"       % "1.4.0",
-    "uk.gov.hmrc" %% "tax-year"                  % "5.0.0"
+    "uk.gov.hmrc" %% "tax-year"                  % "6.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
