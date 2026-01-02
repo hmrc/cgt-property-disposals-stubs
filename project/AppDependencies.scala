f6c34cb7bfd15afc91d9cb6f74ab7eff564950a0
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2026-01-02 16:00:07 +0000

    DLS-12578|Dependencies upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,11 +1,11 @@
 import sbt.*
 
 object AppDependencies {
-  val bootStrapVersion = "9.16.0"
+  val bootStrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootStrapVersion,
-    "uk.gov.hmrc" %% "stub-data-generator"       % "1.5.0",
+    "uk.gov.hmrc" %% "stub-data-generator"       % "1.6.0",
     "uk.gov.hmrc" %% "tax-year"                  % "6.0.0"
   )
 
